64bfce9db7168679746cafa281cd35fd83496dd9
Xuffasch <tristan.lucas@gmail.com>
2021-03-02 10:14:21 +0100

    Add color and size settings to tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'albumBlue': '#004990',
+      }
+    },
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
 }
