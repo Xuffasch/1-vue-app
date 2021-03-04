f5f1715611644db715f117bdab0c84468512642c
Xuffasch <tristan.lucas@gmail.com>
2021-03-04 10:46:34 +0100

    File clean up after WindiCSS install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
-// const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -11,11 +8,7 @@ module.exports = {
     },
     screens: {
       xs: '475px',
-      //   ...defaultTheme.screens,
     },
   },
-  // variants: {
-  //   extend: {},
-  // },
   plugins: [],
 }
