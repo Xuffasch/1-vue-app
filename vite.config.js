60d2ede4a9b569e0e54888d0fc5da205c07acb8a
Xuffasch <tristan.lucas@gmail.com>
2021-03-15 15:08:14 +0100

    Auto generated Service Worker with vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,29 @@ import vue from '@vitejs/plugin-vue'
 
 import WindiCSS from 'vite-plugin-windicss'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue(),
+    WindiCSS(),
+    VitePWA({
+      manifest: {},
+      workbox: {
+        // Custom cach strategy config to include in auto generated sw.js
+        // runtimeCaching: [
+        //   {
+        //     urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+        //     handler: 'CacheFirst',
+        //     options: {
+        //       cacheName: 'vue-images',
+        //     },
+        //   },
+        // ],
+      },
+    }),
+  ],
   server: {
     port: 3010,
   },
