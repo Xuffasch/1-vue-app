aa05f66082b714cac79493d763301023b02ca0a9
Xuffasch <tristan.lucas@gmail.com>
2021-03-16 10:09:24 +0100

    Add section for content display

--- src/views/Versus.vue
+++ src/views/Versus.vue
@@ -11,24 +11,27 @@
       alt="anakin skywalker figurine with a lightsaber"
     />
   </div>
-  <ImageText
-    :input="{
-      src: '/versus/star-wars-kylo-ren-lightsaber-figurine.jpg',
-      alt: 'kylo ren with a lightsaber',
-      versus: 0,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    }"
-  />
-  <ImageText
-    :input="{
-      src: '/versus/star-wars-anakin-skywalker-lightsaber-figurine.jpg',
-      alt: 'anakin skywalker with a lightsaber',
-      versus: 1,
-      description:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    }"
-  />
+  <section>
+    <ImageText
+      :input="{
+        src: '/versus/star-wars-kylo-ren-lightsaber-figurine.jpg',
+        alt: 'kylo ren with a lightsaber',
+        versus: 0,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      }"
+    />
+    <h1>1. Coolest lightsaber color</h1>
+    <ImageText
+      :input="{
+        src: '/versus/star-wars-anakin-skywalker-lightsaber-figurine.jpg',
+        alt: 'anakin skywalker with a lightsaber',
+        versus: 1,
+        description:
+          'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      }"
+    />
+  </section>
 </template>
 
 <script>
